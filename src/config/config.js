1dfcaf6e9c3df92234c31602c60415c23499440e
AYOMITIDE OAJ <36337827+AYOMITIDE-OAJ@users.noreply.github.com>
2023-08-21 14:32:27 +0100

    initial commit

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const WEB_CLIENT_ID = '452097978047-b56dksvr0uiq2ih1md4qjkqlcahvd7cr.apps.googleusercontent.com';
+export const CLOUDINARY_PRESENT_NAME = 'wmgsv8tv';
+export const CLOUDINARY_CLOUD_NAME = 'dfw6lz85o';
+export const STRIPE_PUBLIC_KEY = 'pk_test_51HOg48ApFGBvhObCzqXkTl3xsUUAf7lduboZmh0ZyrKrAeXlSdAK27hvbmG5CkPrarhWbznXsjQywUcmPiq2isLs00M8IGifaQ';
+export const STRIPE_CLOUD_SERVER_URL = 'http://localhost:5000/epicbae-246b2/us-central1/payWithStripe';
+export const IS_STRIPE_LIVE = false;
+
+
